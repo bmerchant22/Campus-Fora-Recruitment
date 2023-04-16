6ede34f2037d26d71c2deacfa9a2c43952886335
bmerchant <bmerchant22@iitk.ac.in>
2023-04-16 22:17:16 +0530

    Commented db

--- pkg/web/handlers.go
+++ pkg/web/handlers.go
@@ -12,14 +12,14 @@ func (srv *Server) AddPost(c echo.Context) error {
 	username := c.FormValue("username")
 	post := c.FormValue("post")
 
-	// Connected to mysql server
+	// Connected to mysql server, user : root, db : fora
 	db, err := sql.Open("mysql", "root:BM@mysql53@tcp(127.0.0.1:3306)/fora")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// Inserted the entered form value into mysql table
+	// Inserted the entered form value into mysql table, named newposts
 	insert, err := db.Query("INSERT INTO `newposts` VALUES(?, ?)", username, post)
 	if err != nil {
 		panic(err)
